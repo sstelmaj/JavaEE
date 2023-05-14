4ad9b801432be39a5f505d853a1658051ad75d4e
JoaquinMaidana <jocomaidana11@gmail.com>
2023-05-14 16:42:47 -0300

    con filtrado y pruebas de etiquetas

--- src/main/java/Presentacion/RSTA.java
+++ src/main/java/Presentacion/RSTA.java
@@ -21,6 +21,8 @@ import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JOptionPane;
 import javax.swing.border.Border;
+import org.fife.ui.autocomplete.*;
+
 
 import org.fife.ui.rtextarea.*;
 import org.fife.ui.rsyntaxtextarea.*;
@@ -61,8 +63,16 @@ public class RSTA extends javax.swing.JInternalFrame  implements ActionListener 
    //    Font font = new Font("Segoe UI", Font.PLAIN, 12);
    //     textArea.setFont(font);
         textArea.setSyntaxEditingStyle(SyntaxConstants.SYNTAX_STYLE_JAVA);
+        
+        CompletionProvider provider = createCompletionProvider();
+
+ 
+        
         //para poder abrir o colapsar metodos
         textArea.setCodeFoldingEnabled(true);
+        
+        AutoCompletion ac = new AutoCompletion(provider);
+        ac.install(textArea);
         RTextScrollPane sp = new RTextScrollPane(textArea);
         jPanel_CP.add(sp);
         
@@ -244,7 +254,7 @@ public class RSTA extends javax.swing.JInternalFrame  implements ActionListener 
             }
         }
     }//GEN-LAST:event_jButton2ActionPerformed
-
+    
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
         // TODO add your handling code here:
              try {
@@ -418,6 +428,38 @@ public class RSTA extends javax.swing.JInternalFrame  implements ActionListener 
     }
         
     }//GEN-LAST:event_jComboBoxLengActionPerformed
+    private CompletionProvider createCompletionProvider() {
+
+      // A DefaultCompletionProvider is the simplest concrete implementation
+      // of CompletionProvider. This provider has no understanding of
+      // language semantics. It simply checks the text entered up to the
+      // caret position for a match against known completions. This is all
+      // that is needed in the majority of cases.
+      DefaultCompletionProvider provider = new DefaultCompletionProvider();
+      provider.setAutoActivationRules(true, "@");
+      // Add completions for all Java keywords. A BasicCompletion is just
+      // a straightforward word completion.
+      provider.addCompletion(new BasicCompletion(provider, "abstract"));
+      provider.addCompletion(new BasicCompletion(provider, "assert"));
+      provider.addCompletion(new BasicCompletion(provider, "break"));
+      provider.addCompletion(new BasicCompletion(provider, "case"));
+      // ... etc ...
+      provider.addCompletion(new BasicCompletion(provider, "transient"));
+      provider.addCompletion(new BasicCompletion(provider, "try"));
+      provider.addCompletion(new BasicCompletion(provider, "void"));
+      provider.addCompletion(new BasicCompletion(provider, "volatile"));
+      provider.addCompletion(new BasicCompletion(provider, "while"));
+
+      // Add a couple of "shorthand" completions. These completions don't
+      // require the input text to be the same thing as the replacement text.
+      provider.addCompletion(new ShorthandCompletion(provider, "sysout",
+            "System.out.println(", "System.out.println("));
+      provider.addCompletion(new ShorthandCompletion(provider, "syserr",
+            "System.err.println(", "System.err.println("));
+
+      return provider;
+
+   }
 
     /**
      * @param args the command line arguments
--- src/main/java/Presentacion/SubirErrorController.java
+++ src/main/java/Presentacion/SubirErrorController.java
@@ -9,27 +9,49 @@ import Logica.Clases.*;
 import Persistencia.Conexion;
 import java.awt.Dimension;
 import java.net.URL;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Locale;
 import java.util.ResourceBundle;
+import java.util.function.Predicate;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.embed.swing.SwingNode;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Bounds;
+import javafx.scene.Scene;
 import javafx.scene.control.Accordion;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TitledPane;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
+import javafx.stage.Stage;
 import javax.swing.JInternalFrame;
 import jdk.nashorn.api.scripting.JSObject;
+import org.fife.ui.autocomplete.AutoCompletion;
+import org.fife.ui.autocomplete.BasicCompletion;
+import org.fife.ui.autocomplete.CompletionProvider;
+import org.fife.ui.autocomplete.DefaultCompletionProvider;
+import org.fife.ui.autocomplete.ShorthandCompletion;
 import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea;
+import org.fife.ui.rtextarea.RTextScrollPane;
 
 /**
  * FXML Controller class
@@ -74,47 +96,120 @@ public class SubirErrorController implements Initializable {
     @FXML
     private Button linkButtonVisualizar1;
     @FXML
-    private TextField linkTextFieldUrl1;
-    @FXML
     private ListView<String> listaEtiquetas;
     
     private SwingNode swingNode;
+    
+    private SwingNode swingNodeConsole;
+    @FXML
+    private TextArea textDescripcion;
+    @FXML
+    private TextField textFieldRepositorio;
+    @FXML
+    private TextField filtroEtiquetas;
+    
+    private  ObservableList<String> items;
+    @FXML
+    private Button botonAgregarEtiqueta;
+    @FXML
+    private ListView<String> etiquetasSeleccionadas;
+    private final String[] palabrasClave = {"auto", "automóvil", "autobús", "autopista"};
+    @FXML
+    private ListView<String> listaCompletado;
+    @FXML
+    private AnchorPane anchorPrueba;
+    @FXML
+    private AnchorPane anchorPrueba2;
+    @FXML
+    private TextArea textPrueba8;
+    @FXML
+    private TextArea text50;
+    
+    private Stage popupStage;
+    
+    private ListView<String> listView;
+    
+    private List<Tecnologia> tecnologias;
+    
+    private ObservableList<String> originalItems;
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        ///Prueba de 
+//        ObservableList<String> items = FXCollections.observableArrayList(
+//                "Elemento 1",
+//                "Elemento 2",
+//                "Elemento 3"
+//        );
+
+        // Crear el componente ListView
+//        listView = new ListView<>(items);
         
-        
-        try {
-            List<Tecnologia> tecnologias = Conexion.getInstance().listaTecnologias();
-            comboTecnologia.getItems().clear();
-            for (Tecnologia tecnologia : tecnologias) {
-                comboTecnologia.getItems().add(tecnologia.getNombre());
+         text50.addEventHandler(KeyEvent.KEY_TYPED, event -> {
+            if (event.getCharacter().equals("@")) {
+            //    openPopup();
+            //    updatePopupPosition();
+                System.out.println("hola");
             }
-        } catch (Exception e) {
-            // Manejo de la excepción
-            e.printStackTrace();
-        }
+        });
+
         
+        llenarTecnologias();
+
+        
+        
+        
+        RSyntaxTextArea rsyntax = new RSyntaxTextArea(20, 60);;
+        RTextScrollPane sp = new RTextScrollPane(rsyntax);
+        SwingNode swingNodeConsole2 = new SwingNode();
+        CompletionProvider provider = createCompletionProvider();
+
+
+        AutoCompletion ac = new AutoCompletion(provider);
+        ac.setShowDescWindow(true);
+        ac.setParameterAssistanceEnabled(true);
+   
+        ac.setAutoCompleteEnabled(true);
+        ac.setAutoActivationEnabled(true);
+        ac.setAutoCompleteSingleChoices(true);
+        ac.setAutoActivationDelay(800);
+         ac.install(rsyntax);
+        swingNodeConsole2.setContent(sp);
+         anchorPrueba2.getChildren().add(swingNodeConsole2);
+         
+         
+         
+         
+        //para poder filtrar luego
         try {
             List<Etiqueta> etiquetas = Conexion.getInstance().listaEtiquetas();
 
-            // Limpiar los elementos existentes en el ListView
-            listaEtiquetas.getItems().clear();
+            // Crear un ObservableList a partir de la lista existente
+             items = FXCollections.observableArrayList();
 
-            // Agregar los elementos a la lista del ListView
+            // Agregar los elementos a la lista ObservableList
             for (Etiqueta etiqueta : etiquetas) {
-                listaEtiquetas.getItems().add(etiqueta.getNombre());
+                items.add(etiqueta.getNombre());
             }
+
+            // Asignar el ObservableList al ListView
+            listaEtiquetas.setItems(items);
+           
         } catch (Exception e) {
             // Manejo de la excepción
             e.printStackTrace();
         }
         
+       filtroEtiquetas.textProperty().addListener((observable, oldValue, newValue) -> {
+            filterListView(listaEtiquetas, newValue, items);
+        });
+          
         
-        JInternalFrame iFrame = new RSTA();
+        
+        JInternalFrame iFrame = new RSTA_TEXT();
         iFrame.setPreferredSize(new Dimension(550,400));
         iFrame.setSize(20, 20);
         iFrame.setVisible(true);
@@ -129,13 +224,15 @@ public class SubirErrorController implements Initializable {
          
          
         try {
-                JInternalFrame iFrameConsole = new RSTA();
+                JInternalFrame iFrameConsole = new RSTA_TEXT();
                 iFrameConsole.setPreferredSize(new Dimension(550,400));
                 iFrameConsole.setSize(20, 20);
                 iFrameConsole.setVisible(true);
+              //  iFrameConsole.setAlwaysOnTop(true);
                 iFrameConsole.setBorder(null);
                 ((javax.swing.plaf.basic.BasicInternalFrameUI) iFrameConsole.getUI()).setNorthPane(null);
-                SwingNode swingNodeConsole = new SwingNode();
+                 
+                swingNodeConsole = new SwingNode();
 
                 swingNodeConsole.setContent(iFrameConsole);
                 scrollConsole.setContent(swingNodeConsole);
@@ -143,23 +240,14 @@ public class SubirErrorController implements Initializable {
                 // Manejo de la excepción
                 e.printStackTrace();
             }
-
+       
          
-//         try {
-//            Iterator<Tecnologia> it = Conexion.getInstance().select(" FROM tecnologia", Tecnologia.class).iterator();
-//            while (it.hasNext()) {
-//                Tecnologia tecnologia = it.next();
-//                // hacer algo con cada objeto tecnologia, por ejemplo mostrar su nombre
-//                System.out.println(tecnologia.getNombre());
-//            }
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
 
       
- 
+        
       
     }    
+    
 
     @FXML
     private void click(ActionEvent event) {
@@ -174,24 +262,157 @@ public class SubirErrorController implements Initializable {
 
     @FXML
     private void clickIngresar(ActionEvent event) {
-    //   Logica.Clases.Error error = new Logica.Clases.Error();
+       Logica.Clases.Error error = new Logica.Clases.Error();
        
-    //   error.setTitulo(textFieldTitulo.getText());
        
+       //buscamos el internal frame del codigo
        JInternalFrame internalFrame = (JInternalFrame) swingNode.getContent();
        
+        //obtenemos el contenido de el internal frame
         if (internalFrame instanceof RSTA) {
-    RSTA rsta = (RSTA) internalFrame;
-    RSyntaxTextArea textArea = rsta.getTextArea();
+            RSTA rsta = (RSTA) internalFrame;
+            RSyntaxTextArea textArea = rsta.getTextArea();
 
-    // Ahora puedes acceder al textArea y realizar operaciones en él
-    String contenido = textArea.getText();
-    System.out.print(contenido);
-    // ... hacer algo con el contenido del textArea
-    } else {
-        // El JInternalFrame no es una instancia de RSTA, maneja el caso según tus necesidades
-    }
+            // Accedemos al text area
+            String contenido = textArea.getText();
+
+            System.out.print(contenido);
+            //seteamos el contenido
+            error.setCodigo(contenido);
+        } else {
+            
         }
+        
+        error.setTitulo(textFieldTitulo.getText());
+        
+        JInternalFrame internalFrameConsola = (JInternalFrame) swingNodeConsole.getContent();
+       
+        //obtenemos el contenido de el internal frame
+        if (internalFrameConsola instanceof RSTA) {
+            RSTA rsta = (RSTA) internalFrameConsola;
+            RSyntaxTextArea textArea = rsta.getTextArea();
+
+            // Accedemos al text area
+            String contenido = textArea.getText();
+
+            System.out.print(contenido);
+            //seteamos el contenido
+            error.setConsola(contenido);
+        } else {
+            
+        }
+        
+        error.setDescripcion(textDescripcion.getText());
+        
+    //    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy", new Locale("es")); (util para despues o capaz el input fecha lo modifica)
+
+        // Formatear LocalDate a String en español
+    //    String fechaFormateada = localDate.format(formatter);
+        Date date = Date.from(inputFecha.getValue().atStartOfDay(ZoneId.systemDefault()).toInstant());
+        System.out.println(date);
+        error.setFechaSubida(date);
+        error.setLink(linkTextFieldUrl.getText());
+        error.setRepositorio(textFieldRepositorio.getText());
+        
+        Conexion.getInstance().persist(error);
+        
+    }
+
+    @FXML
+    private void agregarEtiqueta(ActionEvent event) {
+        
+        String selectedItem = listaEtiquetas.getSelectionModel().getSelectedItem();
+
+        // Verifica si se seleccionó algún elemento
+        if (selectedItem != null) {
+            // Agrega el elemento a la ListView de destino
+            etiquetasSeleccionadas.getItems().add(selectedItem);
+            
+            
+        }
+    }
+    private void llenarTecnologias() {
+       try {
+            List<Tecnologia> tecnologias = Conexion.getInstance().listaTecnologias();
+            comboTecnologia.getItems().clear();
+            for (Tecnologia tecnologia : tecnologias) {
+                comboTecnologia.getItems().add(tecnologia.getNombre());
+            }
+        } catch (Exception e) {
+            // Manejo de la excepción
+            e.printStackTrace();
+        }
+    }
+    
+    private void filterListView(ListView<String> listView, String filterText, ObservableList<String> items) {
+    // Crear un nuevo filtro utilizando el texto ingresado
+    Predicate<String> filter = item ->
+            item.toLowerCase().contains(filterText.toLowerCase());
+
+    // Filtrar la lista de etiquetas utilizando el filtro
+    List<String> filteredList = items.filtered(filter);
+
+    // Actualizar la lista del ListView con los elementos filtrados
+    listView.setItems(FXCollections.observableArrayList(filteredList));
+}
+    
+    private CompletionProvider createCompletionProvider() {
+
+     
+      DefaultCompletionProvider provider = new DefaultCompletionProvider();
+        provider.setAutoActivationRules(true, "@");
+
+      // Add completions for all Java keywords. A BasicCompletion is just
+      // a straightforward word completion.
+      provider.addCompletion(new BasicCompletion(provider, "abstract"));
+      provider.addCompletion(new BasicCompletion(provider, "assert"));
+      provider.addCompletion(new BasicCompletion(provider, "break"));
+      provider.addCompletion(new BasicCompletion(provider, "case"));
+      // ... etc ...
+      provider.addCompletion(new BasicCompletion(provider, "transient"));
+      provider.addCompletion(new BasicCompletion(provider, "try"));
+      provider.addCompletion(new BasicCompletion(provider, "void"));
+      provider.addCompletion(new BasicCompletion(provider, "volatile"));
+      provider.addCompletion(new BasicCompletion(provider, "while"));
+
+      // Add a couple of "shorthand" completions. These completions don't
+      // require the input text to be the same thing as the replacement text.
+      provider.addCompletion(new ShorthandCompletion(provider, "sysout",
+            "System.out.println(", "System.out.println("));
+      provider.addCompletion(new ShorthandCompletion(provider, "syserr",
+            "System.err.println(", "System.err.println("));
+
+      return provider;
+
+   }
+//    private void openPopup() {
+//        // Crear un nuevo escenario (Stage) para la ventana emergente
+//         popupStage = new Stage();
+//
+//        // Configurar el contenido de la ventana emergente (puedes usar un nuevo archivo FXML y controlador si es necesario)
+//        // Aquí se muestra un ejemplo simple de contenido usando un StackPane con una etiqueta
+//        popupStage.setAlwaysOnTop(true);
+//        StackPane popupRoot = new StackPane(listView);
+//        Scene popupScene = new Scene(popupRoot, 200, 100);
+//        popupStage.setScene(popupScene);
+//
+//        // Mostrar la ventana emergente
+//        popupStage.show();
+//    }
+    
+//    private void updatePopupPosition() {
+//        int caretPosition = text50.getCaretPosition();
+//        int lineNumber = text50.getText().substring(0, caretPosition).split("\n").length;
+//        int columnIndex = caretPosition - text50.getText().lastIndexOf('\n', caretPosition - 1) - 1;
+//
+//        // Calcular las coordenadas de la ventana emergente
+//        double x = text50.localToScreen(text50.getBoundsInLocal()).getMinX() + columnIndex * 10;
+//        double y = text50.localToScreen(text50.getBoundsInLocal()).getMinY() + lineNumber * 20 + 20;
+//
+//        // Establecer la posición de la ventana emergente
+//        popupStage.setX(x);
+//        popupStage.setY(y);
+//    }
 
     
 }
