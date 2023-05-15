dfc2b1b480c30af234c1a76d0ce0fdedf7a1ef9e
paulo-ruiz <paulo.ruizanderson@gmail.com>
2023-05-15 18:45:03 -0300

    Avance en funcionalidad de Detalle solucion

---
+++ src/main/java/Logica/Controladores/SolucionController.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Singleton.java to edit this template
+ */
+package Logica.Controladores;
+
+import Logica.Clases.Solucion;
+import Persistencia.Conexion;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+public class SolucionController {
+    
+    private SolucionController() {
+    }
+    
+    public static SolucionController getInstance() {
+        return SolucionControllerHolder.INSTANCE;
+    }
+    
+    private static class SolucionControllerHolder {
+        private static final SolucionController INSTANCE = new SolucionController();
+        private static final Conexion con=Conexion.getInstance();
+    }
+    
+     public List<Solucion> obtenerSolucion(String id){
+        List<Solucion> resultado=null;
+        EntityManager em= SolucionControllerHolder.con.getEntity();
+        em.getTransaction().begin();
+        try{
+            Query q = em.createNativeQuery("SELECT * from Solucion where id="+id, Solucion.class);
+            resultado=q.getResultList();
+            em.getTransaction().commit();  
+    }catch(Exception e){
+        em.getTransaction().rollback();
+    }
+      return resultado;  
+    }
+}
